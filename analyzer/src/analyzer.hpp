2df0f17cdc34a8e9ce430b8aaa3f59c644d6e7fa
kangdongh <kangdongh@snu.ac.kr>
2016-10-20 16:54:56 +0900

    omitted files added

---
+++ analyzer/src/analyzer.hpp
@@ -0,0 +1,54 @@
+#ifndef __DRONE_ANALYZER_HPP__
+#define __DRONE_ANALYZER_HPP__
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include "types.h"
+
+#define PARR_LENGTH 3
+
+
+enum STEREOTYPE{
+	MESSAGE_STREAM,
+	VIDEO_STREAM,
+	IMAGE_STREAM // not used yet
+} // for debuging!
+
+class Analyzer
+{
+public:
+	Analyzer();
+	bool Initialize(); // false - fail to set the message queues
+	void Prepare(); // takeoff & lift
+	void Run(); // run only one step...? TBD
+	void Test(); // function for testing
+	void Set_video_source(char name[]);
+	void PrintInfo();
+	void SendCommand(DRONE_COMMAND cmd);
+private:
+	bool ReceiveSVO();
+	bool ReceiveStereo();
+	bool ReceiveNavdata(); // failed = false
+	
+	DRONE_COMMAND NormalMode();
+	DRONE_COMMAND SwerveMode();
+	DRONE_COMMAND ReturnMode();
+	
+	void ProcessStereo();
+	void PrintProcessed();
+	key_t stereo_key;
+	key_t svo_key;
+	key_t drone_key;
+	
+	int state; // 0 : go straight , 1 : swerve, 2 : retrun to origin route
+	// 0 -> 1 <-> 2 -> 0 (FSM), other transition is not permitted
+	NAVDATA nav_data;
+	SVODATA svo_data;
+	cv::Mat stereo_data;
+	unsigned char processed_data[PARR_LENGTH][PARR_LENGTH];
+	STEREOTYPE stereo_source;
+	cv::VideoCapture* video_source;
+}
+
+//__DRONE_ANALYZER_HPP__
+#endif
---
+++ analyzer/src/types.c
@@ -0,0 +1,43 @@
+#include "types.h"
+
+void print_cmd(DRONE_COMMAND cmd)
+{
+	switch(cmd)
+	{
+		case TAKEOFF:
+			printf("Take Off\n");
+			break;
+		case LAND:
+			printf("Land\n");
+			break;
+		case HOVERING:
+			printf("Hovering\n");
+			break;
+		case SPINR:
+			printf("Spin right\n");
+			break;
+		case SPINL:
+			printf("Spin left\n");
+			break;
+		case MOVEF:
+			printf("Move forward\n");
+			break;
+		case MOVER:
+			printf("Move to rightside\n");
+			break;
+		case MOVEL:
+			printf("Move to leftside\n");
+			break;
+		case MOVEB:
+			printf("Move backward\n");
+			break;
+		case MOVEU:
+			printf("Go higher (lift)\n");
+			break;
+		case MOVED:
+			printf("Go lower\n");
+			break;
+		default:
+			printf("Unknown command %d\n", cmd);
+	}
+}
